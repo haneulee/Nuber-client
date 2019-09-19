b210aa4ff1fefd401739a6d92821058e14ec86d8
Haneul Lee <lovesky4294@gmail.com>
2019-09-20 00:55:07 +0900

    apollo setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { ApolloProvider } from "react-apollo";
+import client from "./apollo"
+
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root') as HTMLElement
 );
