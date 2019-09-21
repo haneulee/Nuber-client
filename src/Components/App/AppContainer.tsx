5b8b22d640629555718cab021f3984cdfc78e298
Haneul Lee <lovesky4294@gmail.com>
2019-09-21 16:39:03 +0900

    created global styles

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { graphql } from "react-apollo";
+import theme from "../../theme";
+import { ThemeProvider } from "../../typed-components";
 import AppPresenter from "./AppPresenter";
 import { IS_LOGGED_IN } from "./AppQueries";
 
-const AppContainer = ({ data }) => {
-    return <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
-};
+// tslint:disable-next-line
+// injectGlobal`
+//     ${reset}
+// `;
+
+const AppContainer = ({ data }) => <ThemeProvider theme={theme}>
+    <>
+        <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+    </>
+</ThemeProvider>
+
+
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
-import * as React from 'react';
-import { ApolloProvider } from "react-apollo";
+import React from 'react';
 import ReactDOM from 'react-dom';
+import GlobalStyle from "src/global-styles";
 import client from "./apollo"
+import { ApolloProvider } from "react-apollo";
 import App from './Components/App';
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
+    <GlobalStyle />
   </ApolloProvider>,
   document.getElementById("root") as HTMLElement
 );
