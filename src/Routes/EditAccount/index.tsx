4a4d7ee099d7f879546367994b0827cb4e25227b
Haneul Lee <lovesky4294@gmail.com>
2019-10-13 15:24:17 +0900

    edit account including uploading profile photo

--- src/Routes/EditAccount/index.tsx
+++ src/Routes/EditAccount/index.tsx
@@ -1,5 +1,2 @@
-import React from "react";
-
-const test = () => <span>blaafaf</span>
-
-export default test;
+import EditAccountContainer from "./EditAccountContainer";
+export default EditAccountContainer;
